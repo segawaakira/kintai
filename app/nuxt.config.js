37cd0324ff969959cb8b3fcbf3b9dbf7bfbaf3b6
segawa <>
2021-10-05 09:28:16 +0900

    feature:vuex-persistedstateで、project選択のstoreを永続化

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -31,7 +31,8 @@ const nuxtConfig = {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/firebase'
+    '@/plugins/firebase',
+    '@/plugins/persistedstate'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
