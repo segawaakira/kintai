b891f805088d44e41d8d5ed99ce0bbfbf224a954
segawa <>
2021-10-06 02:34:22 +0900

    feature:メニュー

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -38,36 +38,12 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
-      {{ store.state.project }}
       <v-select
+        v-model="selectedProject"
         :items="projects"
         filled
         label="Select project"
@@ -79,69 +55,9 @@
       <hr>
 
       <v-container>
-        <v-btn
-          color="primary"
-          nuxt
-          to="/signup"
-        >
-          signup
-        </v-btn>
-        <v-btn
-          color="primary"
-          nuxt
-          to="/login"
-        >
-          login
-        </v-btn>
-        <v-btn
-          color="primary"
-          nuxt
-          to="/input"
-        >
-          input
-        </v-btn>
-        <v-btn
-          color="primary"
-          nuxt
-          to="/calendar"
-        >
-          calendar
-        </v-btn>
-        <v-btn
-          color="primary"
-          nuxt
-          to="/detail"
-        >
-          detail
-        </v-btn>
-        <v-btn
-          color="primary"
-          nuxt
-          to="/projects"
-        >
-          projects
-        </v-btn>
-        <hr>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -157,7 +73,9 @@ import firebase from 'firebase'
 
 export default defineComponent({
   setup (_props, context: any) {
+    const isSignedIn: Ref<Boolean> = ref(false)
     const projects: Ref<any> = ref([])
+    const selectedProject: Ref<any> = ref({})
     const currentUser: Ref<any> = ref(null)
     const store = useStore()
     const theme: Ref<boolean> = ref(true)
@@ -175,8 +93,11 @@ export default defineComponent({
     onMounted(() => {
       // @ts-ignore
       theme.value = store.state.dark
+      // @ts-ignore
+      selectedProject.value = store.state.project
       firebase.auth().onAuthStateChanged((data) => {
         if (data) {
+          isSignedIn.value = true
           currentUser.value = firebase.auth().currentUser
           const db = firebase.firestore()
 
@@ -204,25 +125,43 @@ export default defineComponent({
     )
 
     return {
-      clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: 'signup',
+          to: '/signup',
+          isLogin: false
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'login',
+          to: '/login',
+          isLogin: false
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'input',
+          to: '/input',
+          isLogin: true
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'calendar',
+          to: '/calendar',
+          isLogin: true
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'projects',
+          to: '/projects',
+          isLogin: true
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Vuetify.js',
+      isSignedIn,
+      selectedProject,
       projects,
       currentUser,
       onChangeProject,
